3bb8c21c9eae732c7cadc3ee0a7bd8fc810409ed
Stephen Humphries <stephen.humphries@shopify.com>
2022-11-18 13:38:59 -0500

    Update to use new quick-access menu

--- meet-default-mic.js
+++ meet-default-mic.js
@@ -1,19 +1,7 @@
-const optionsLabel = "More options";
-const settingsLabel = "Settings";
-const microphoneLabel = "Select Microphone";
-const microphoneBlocked = "Microphone is blocked";
-const closeLabel = "Close dialog";
+const microphoneLabel = "Microphone: ";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function getOptions() {
-    return document.querySelector(`button[aria-label='${optionsLabel}']`);
-}
-
-function getSettings() {
-    return document.evaluate(`//span[text()='${settingsLabel}']`, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
 }
 
 function getMicrophone(microphoneName) {
@@ -21,67 +9,21 @@ function getMicrophone(microphoneName) {
 }
 
 function getMicrophoneMenu() {
-    let foo = document.querySelector(`[data-aria-label='${microphoneLabel}']`);
-    if (foo) {
-        let bar = foo.querySelector("[aria-haspopup='listbox']");
-        if (bar.innerText != microphoneBlocked) {
-            return bar;
-        }
-        return null;
-    }
-    return null;
-}
-
-function getClose() {
-    let foo = document.querySelector(`[aria-label='${settingsLabel}']`);
-    if (foo) {
-        return foo.querySelector(`[aria-label='${closeLabel}']`);
-    }
-    return null;
+    return document.querySelector(`[aria-label^='${microphoneLabel}']`);
 }
 
 async function setMicrophone(microphoneName) {
-    // Open options menu
-    do {
-        await sleep(100);
-    } while (!getOptions());
-    getOptions().click();
-
-    // Open settings pane
-    do {
-        await sleep(100);
-    } while (!getSettings());
-    getSettings().click();
-
     // Open microphone selector
     do {
         await sleep(100);
     } while (!getMicrophoneMenu());
     getMicrophoneMenu().click();
 
-    await sleep(100);
-    if (!getMicrophone(microphoneName)) {
-        // Wait for the desired microphone to be connected
-        do {
-            await sleep(100);
-        } while (!getMicrophone(microphoneName));
-
-        // Open microphone selector (again)
-        do {
-            await sleep(100);
-        } while (!getMicrophoneMenu());
-        getMicrophoneMenu().click();
-    }
-
     // Select microphone
     do {
         await sleep(100);
     } while (!getMicrophone(microphoneName));
     getMicrophone(microphoneName).click();
-
-    // Close settings pane
-    await sleep(250);
-    getClose().click();
 }
 
 browser.storage.local.get("microphoneName").then((item) => setMicrophone(item.microphoneName));
